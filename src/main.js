0821ef54b23f043c1b172f3197d92f7339a47688
Tawnee Harris <tawneeh@icloud.com>
2020-11-18 21:53:39 -0800

    update UI logic

--- src/main.js
+++ src/main.js
@@ -2,26 +2,48 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './services/weather-service.js'
+import WeatherService from './services/weather-service.js';
+import GiphyService from './services/giphy-service';
 
 function clearFields() {
   $('#location').val("");
-  $('.showErrors').text("");
-  $('.showHumidity').text("");
-  $('.showTemp').text("");
+  $('.show-errors').text("");
+}
+
+function displayWeatherDescription(description) {
+  $('.weather-description').text(`The weather is ${description}!`);
+}
+
+function displayGif(response) {
+  const url = response.data[0].images.downsized.url;
+  $('.show-gif').html(`<img src='${url}'>`);
+}
+
+function displayErrors(error) {
+  $('.show-errors').text(`${error}`);
 }
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    let promise = WeatherService.getWeather(city);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-    });
+    WeatherService.getWeather(city)
+      .then(function(weatherResponse) {
+        if (weatherResponse instanceof Error) {
+          throw Error(`OpenWeather API error: ${weatherResponse.message}`);
+        }
+        const weatherDescription = weatherResponse.weather[0].description;
+        displayWeatherDescription(weatherDescription);
+        return GiphyService.getGif(weatherDescription);
+      })
+      .then(function(giphyResponse) {
+        if (giphyResponse instanceof Error) {
+          throw Error(`Giphy API error: ${giphyResponse.message}`);
+        }
+        displayGif(giphyResponse);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
   });
 });
